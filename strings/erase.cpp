1407473b584e88c3075590ff97cc5555607e0beb
Mohd Owais <mohdowais762@gmail.com>
2021-10-14 12:56:15 +0530

    another commit

---
+++ strings/erase.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    string s1 = "nilcompoop";
+    
+    cout<<s1.erase(2,3)<<endl;
+    return 0;
+}
---
+++ strings/find.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    string s1 = "nilcompoop";
+    cout<<s1.find("poo");
+    return 0;
+}
