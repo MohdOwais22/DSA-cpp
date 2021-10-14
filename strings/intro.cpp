1e45e670aadc2390a7abf631ec1e0275065604a5
Mohd Owais <mohdowais762@gmail.com>
2021-10-14 12:49:17 +0530

    initial commit

---
+++ strings/intro.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+ int main(){
+
+    //  string str;
+    //  string str1(5,'n');
+
+    //  cin>> str;
+    //  cout<<str1<<endl;
+
+    // getline(cin,str);
+
+    // cout<<str<<endl;
+    string s1 = "fam";
+    string s2 = "ily";
+
+    s1 = s1+s2;
+    cout<< s1<<endl;
+
+
+
+     
+     return 0;
+ }
