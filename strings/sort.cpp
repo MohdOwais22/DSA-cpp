777150a1dc4a6b599bfeaa0f778c7a7cfde49356
Mohd Owais <mohdowais762@gmail.com>
2021-10-14 13:09:17 +0530

    string sort

---
+++ strings/sort.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s = "alkjdiaewoiakdnfkl";
+     sort(s.begin(), s.end());
+    cout<<s;
+    return 0;
+}
